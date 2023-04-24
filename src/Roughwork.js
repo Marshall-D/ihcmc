a04588c11b01b9cc5617a372d3a1a39727cd0c05
d1 <www.didowu@gmail.com>
2023-04-24 18:08:08 +0100

    added visa html

--- src/Roughwork.js
+++ src/Roughwork.js
@@ -2,15 +2,174 @@
 
 function Basic() {
   return (
-    <p>
-      For general inquiry about the&nbsp;International Healthcare and Medical
-      Conference, complete the form below.
-      <br />
-      We will usually get back to you via email within 24 hrs.
-      <br />
-      For urgent inquiry do contact the organizers&nbsp;via WhatsApp :&nbsp;+1
-      587 760 3549
-    </p>
+    <div class="content_body_inner_2">
+      <p>
+        <strong>
+          <span style={{ color: "rgb(245, 10, 10)" }}>
+            PLEASE READ CAREFULLY
+          </span>
+        </strong>
+      </p>
+      <p>
+        <a href="http://www.cic.gc.ca/english/visit/visas.asp" target="_blank">
+          Most visitors need an Electronic Travel Authorization or a visa to
+          travel to Canada.
+          <br />
+          <br />
+        </a>
+        Some people may only need their valid passport.
+        <br />
+        <span style={{ color: "rgb(250, 0, 0)" }}>
+          <strong></strong>
+          <strong>
+            IMPORTANT INFORMATION ON VISAS AND ENTRY INTO CANADA
+            <br />
+          </strong>
+        </span>
+      </p>
+      <p>
+        After you have registered for the conference, if you need to apply for a
+        visa please request:{" "}
+      </p>
+      <p>
+        <strong>1)</strong> Official letter of recognition of IHCMC 2023
+        provided by the Canada Border Services Agency to us for dispatch to
+        registered delegates.
+      </p>
+      <p>
+        <strong>2)</strong> A letter of invitation to attend the IHCMC 2023
+        containing The Immigration, Refugees and Citizenship Canada Special
+        Event Code.
+      </p>
+      <p>
+        Attendance at a conference such as&nbsp;IHCMC 2023 is considered to be a
+        cross-border business activity and therefore you will be considered a
+        business visitor.
+        <br />
+        International delegates attending the IHCMC 2023 who require a visa to
+        enter Canada will be issued a <strong>Business Visitor</strong> Visa by
+        the Canada High Commission in their home country.
+      </p>
+      <p>
+        <a
+          href="https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/business.html"
+          target="_blank"
+        >
+          Government of Canada Information on visiting Canada on business
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/business.html"
+          target="_blank"
+        ></a>
+        The Canada Border Services Agency (CBSA) International Events and
+        <br />
+      </p>
+      <p>
+        Convention Services Program (IECSP) has officially recognised the IHCMC
+        2023.
+      </p>
+      <p>
+        To facilitate Canadian border procedures it is recommended that
+        participants present a copy of the official letter of recognition
+        provided by the Canada Border Services Agency (CBSA) – International
+        Events and Convention Services Program (IECSP) to a CBSA Border Services
+        Officer upon their arrival to Canada.
+      </p>
+      <p>
+        The IHCMC 2023 has been registered with The Special Events Unit of
+        Immigration, Refugee and Citizenship Canada (IRCC).
+      </p>
+      <p>
+        {" "}
+        If you require a visa we strongly advise that after registering for the
+        IHCMC you request a “letter of invitation” to the Conference. The letter
+        of invitation will contain The Immigration, Refugees and Citizenship
+        Canada’ Special Event Code for the event. The “letter of invitation”
+        will facilitate your entry into Canada and the processing of visa
+        applications. The Special Events Unit of Immigration, Refugee and
+        Citizenship Canada (IRCC) and the Canadian Border Services Agency will
+        have access to a copy of the format of the letter of invitation. The
+        “letter of invitation” will also facilitate the processing of visa
+        applications.
+      </p>
+      <p>
+        <strong>Please note that following the&nbsp;</strong>
+        <strong>
+          The Immigration, Refugees and Citizenship Canada policy, an invitation
+          letter as well as the letter of recognition will only be issued to
+          registered delegates who have confirmed their place in the conference,
+          by paying the registration fee.
+        </strong>
+      </p>
+      <p>
+        <strong>Submission of Visa Applications:</strong>
+      </p>
+      <p>
+        Canada does not have a visa office in every country so it is important
+        that delegates visit the<strong>&nbsp;</strong>
+        <a
+          href="https://www.canada.ca/en/immigration-refugees-citizenship/corporate/contact-ircc/offices/international-visa-offices.html"
+          target="_blank"
+        >
+          <strong>
+            website of the visa office responsible for processing their visa
+            applications
+          </strong>
+        </a>
+        . Information is available on the IRCC website on how to submit a visa
+        application and the documentation required.
+      </p>
+      <p>
+        Delegates are encouraged to submit their visa applications well in
+        advance of the date of the event.
+      </p>
+      <p>Electronic Travel Authorization (eTA)</p>
+      <p>
+        Visa-exempt foreign nationals are expected to have an&nbsp;
+        <a
+          href="https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/eta.html"
+          target="_blank"
+        >
+          <strong>Electronic Travel Authorization (eTA</strong>)
+        </a>{" "}
+        to fly to or transit through Canada.&nbsp;
+        <a
+          href="https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/entry-requirements-country.html#eta-exemptions"
+          target="_blank"
+        >
+          <strong>Exceptions</strong>
+        </a>{" "}
+        include U.S. citizens, and travelers with a valid Canadian visa.&nbsp;
+      </p>
+      <p>
+        <strong>NEW – Biometrics (Global Expansion)</strong>
+      </p>
+      <p>
+        Depending on your nationality, you may be required to give your
+        biometrics when you apply for the Canada visa.
+      </p>
+      <p>
+        Starting July 31, 2018 the biometric requirement became mandatory for
+        nationals of Europe, Middle East and Africa, and
+      </p>
+      <p>
+        Starting December 31, 2018 the biometric requirement became mandatory
+        for nationals of Asia, Asia Pacific and the Americas.
+      </p>
+      <p>
+        For additional information on whether or not you require a biometric,
+        please refer to our
+        <a
+          href="https://www.canada.ca/en/immigration-refugees-citizenship/campaigns/biometrics/facts.html"
+          target="_blank"
+        >
+          {" "}
+          fast facts about Biometrics.
+        </a>
+      </p>
+    </div>
   );
 }
 
