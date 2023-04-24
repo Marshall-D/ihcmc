a77ac083632e3feff0e9fab1a8cddf8de13603e7
d1 <www.didowu@gmail.com>
2023-04-24 14:10:12 +0100

    added files and folders

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer_wrapper_1">
+      <footer role="contentinfo" className="content_info">
+        <div className="custom_footer">
+          <p>Copyright © 2019-2023. All Rights Reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+  
+  export default Footer;
