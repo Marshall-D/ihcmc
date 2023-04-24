0f57937f23a773446311492f7382a16d17b13646
d1 <www.didowu@gmail.com>
2023-04-24 18:43:55 +0100

    added travel html

--- src/Roughwork.js
+++ src/Roughwork.js
@@ -4,188 +4,129 @@ function Basic() {
   return (
     <div class="content_body_inner_2">
       <p>
-        All relevant stakeholders are welcome to submit your original research
-        papers to the International Healthcare and Medicine Conference which
-        integrates the disciplines towards a better society. The conference is a
-        platform for addressing the various healthcare and medical concerns by
-        discussing and working together towards enhancing the well-being for all
-        at all ages.
+        <strong>The IHCMC 2023 will take place in an around&nbsp;</strong>
+        <strong>
+          2478 Pegasus Rd NE, Calgary, AB T2E 8G8, Canada
+          <br />
+        </strong>
       </p>
       <p>
-        Your original research abstracts, should be made according to the issued
-        format in the Abstract Submission Guidelines prior to the Abstract
-        Submission Deadline 15 May 2023. All submitted research papers, will be
-        evaluated by a group of experts in the related disciplines in accordance
-        with the international criteria. All the assented will be eligible for
-        presentation at the conference and should be registered not later than
-        24 hrs. of receiving an acceptance notification.
+        <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/images/39549046/huge/venuev.png" />
       </p>
       <p>
-        The following are the conference tracks for abstract and paper
-        submission, but not limited to:
+        <strong>
+          <br />
+        </strong>
       </p>
       <p>
         <br />
       </p>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              A. Research Issues– Computers and Primary Care
-              <br />– Clinical Data Visualisation Standards
-              <br />– e-Health Architectures
-              <br />– Healthcare Data Architecture and Terminology Standards
-              <br />– Federated Electronic Health Records
-              <br />– Personalized Medicine
-              <br />– Health Informatics and Education
-              <br />– Human Computer Interaction
-              <br />– Infrastructure and Architecture
-              <br />– Internet and Medicine
-              <br />– Interoperability issues
-              <br />– IT and Patient Care
-              <br />– Nursing Informatics
-              <br />– RFID and localization techniques
-              <br />– Usability and Ubiquity in e-Health
-              <br />– e-Health Virtual Communities
-              <br />– Business Process Management Systems
-              <br />– Second Life for Healthcare Support and Education
-            </td>
-            <td>
-              B. Management Issues– Case Studies
-              <br />– Management Change
-              <br />– Confidentiality and Privacy
-              <br />– e-Health Collaborative Strategies and Techniques
-              <br />– e-Training
-              <br />– Healthcare Management Dashboards
-              <br />– Legal issues
-              <br />– Balanced scorecards models to improve Hospital Performance
-              and Productivity
-              <br />– Business Intelligence in Healthcare
-              <br />– e-Health to improve Healthcare Quality and Patient Safety.
-              <br />– Healthcare Information Systems Regulatory issues
-              <br />– Security in e-Health
-              <br />– Service Models
-              <br />– Social implications
-              <br />– Stakeholders involvement
-            </td>
-          </tr>
-        </tbody>
-      </table>
       <p>
-        C. Applications– Clinical Information Systems
-        <br />– Data Mining and Clinical Studies
-        <br />– Medical Guidelines
-        <br />– e-Health Decision Support Systems
-        <br />– e-Logistics and e-Pharmacy
-        <br />– Intelligent Medical Systems
-        <br />– Mobile Applications
-        <br />– Patient Electronic Health Records
-        <br />– Healthcare Portals to inform and connect Patients with
-        Physicians
-        <br />– Patients and Public Health
-        <br />– Social Networks in Healthcare contexts
-        <br />– e-Health Marketing
-        <br />– e-Procurement and e-Commerce
-        <br />– Telemedicine
-        <br />– Automatic Identification and Data Collector Systems
-        <br />– Unified data processing and communication Systems
-        <br />
+        Calgary International Airport, branded as YYC Calgary International
+        Airport, is an international airport that serves the city of Calgary,
+        Alberta, Canada. It is located approximately 17 km northeast of downtown
+        and covers an area of 21.36 km².
         <br />
       </p>
       <p>
-        <strong>Two-Track Submissions</strong>
+        <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/images/25387263/huge/images.jpg" />
       </p>
       <p>
-        Conference submissions follow two tracks: full conference papers and
-        extended abstracts. All accepted full papers will be published as a
-        volume in the Proceedings of IHCMC . The best full papers will also be
-        invited for submission to a special issue of Medical Image Analysis, a
-        top journal in the field.
+        The Calgary International Airport is the closest airport to the
+        conference venue.
+        <br />
+        CALGARY AIRPORT SHUTTLE SERVICE
       </p>
       <p>
-        <strong>&nbsp;&nbsp;For the Co-Authors</strong>
+        Allied Airport Shuttle has the largest fleet of vehicles in the greater
+        Calgary and surrounding area to meet any transportation need. They have
+        over 40 years of experience providing quality service to the public,
+        business community, schools and sports teams with safe, clean and
+        professionally-driven vehicles.
       </p>
       <p>
-        Please note a single registration permits only one person to attend the
-        conference
-      </p>
-      <p>
-        If the co-authors would like to attend the conference their registration
-        and payment is required to be made independently.
-      </p>
-      <p>
-        However, they will be given a discount of 15% from Non-Presenter’s
-        (Listener’s) registration fee.
-      </p>
-      <p>
-        The certificate will be issued for the co-authors upon their
-        registration/payment for the conference.
-      </p>
-      <p>
-        Please make prior communications with the organizing committee regards
-        to this matter to enjoy the benefit of the discount.
+        Allied Airport Shuttle provides affordable, regularly scheduled
+        transportation, in fully automated GPS dispatched shuttle buses, between
+        designated points within the City of Calgary and the Calgary
+        International Airport.&nbsp;
         <br />
       </p>
       <p>
-        <strong>Submission guidelines&nbsp;</strong>
+        <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/images/25387326/huge/Transit_1.png" />
       </p>
       <p>
-        The Conference will be composed of several types of contributions:
+        Delegates who notify the organizers of their itinerary not later than
+        March 25, 2023 can benefit from the international delegate airport
+        <br />
+        pickup from the Calgary International Airport to designated hotels
+        booked through the organizers.
+      </p>
+      <p>
         <br />
       </p>
-      <ul>
+      <p>
+        <strong>Accommodation</strong>
+      </p>
+      <p>
+        We encourage you to book your accommodation as early as possible. The
+        conference dates are within the high peak season and therefore rooms
+        will be limited unless you book early. We recommend booking your
+        accommodation before March 25, 2023.
+      </p>
+      <p>
+        <strong>Options for accommodation</strong>
+      </p>
+      <p>
+        Options for low-cost accommodation for the dates of the conference are
+        limited. If you require accommodation please contact the organizers
+      </p>
+      <p>
+        There are a number of hotels in close proximity to the event venue
+        <br />
+      </p>
+      <p>
+        &nbsp;Delta Bow Valley - 209-4th Avenue Southeast Calgary, AB (403)
+        266-1980
+      </p>
+      <p>
         {" "}
-        <p></p>
-        <li>
-          Full Papers – These include mainly accomplished research results and
-          have 8 pages at the maximum (5,000 words).
-        </li>
-        <li>
-          Short Papers – These are mostly composed of work in progress reports
-          or fresh developments and have 4 pages at maximum (2,500 words).
-        </li>
-        <li>
-          Reflection Papers – These might review recent research literature
-          pertaining to a particular problem or approach, indicate what the
-          findings suggest, and/or provide a suggestion – with rationale and
-          justification – for a different approach or perspective on that
-          problem. Reflection papers might also analyze general trends or
-          discuss important issues in topics related to the conference. These
-          have 4 pages at maximum (2,500 words).
-        </li>
-        <li>
-          Posters / Demonstrations – These contain implementation information or
-          work-in-progress and have two pages at maximum (1,250 words) besides
-          the poster itself (or demonstration) that will be exposed at the
-          conference.
-        </li>
-        <li>
-          Tutorials – Tutorials can be proposed by scholars or company
-          representatives. A proposal of maximum 250 words is expected.
-        </li>
-        <li>
-          Panels – Discussions on selected topics will be held. A proposal of
-          maximum 250 words is expected.
-        </li>
-        <li>
-          Invited Talks – These will be made of contributions from well-known
-          scholars and company representatives. An abstract will be included in
-          the conference proceedings.
-        </li>
-        <li>
-          Panels – Discussions on selected topics will be held. A proposal of
-          maximum 250 words is expected.
-        </li>
-        <li>
-          Doctoral Consortium – A Doctoral Consortium will discuss in group,
-          individual projects and on going work of PhD students. Prospective
-          students should send a report of their PhD projects and work so far
-          with a maximum of 4 pages (2,500 words).
-        </li>
-      </ul>
+        International Hotel Suites - 220 4 Avenue Southwest Calgary, AB (403)
+        265-9600
+      </p>
       <p>
-        Authors may submit their work following the guidelines via email : ihcmc
-        (at) humanoid.net
+        {" "}
+        The Westin Calgary - 320 4 Avenue Southwest Calgary, AB (403) 266-1611
+      </p>
+      <p>
+        {" "}
+        Sheraton Suites Calgary - 255 Barclay Parade Southwest Calgary, AB (403)
+        266-7200
+      </p>
+      <p>
+        {" "}
+        Regency Suites Calgary - 610 4th Avenue SW Calgary, AB T2P 0K1 (403)
+        231-1000
+      </p>
+      <p>
+        Sandman Hotel Downtown - 888 7 Avenue Southwest, Calgary (403) 237-8626
+        <br />
+      </p>
+      <p>
+        {" "}
+        Holiday Inn Express Downtown - 1020 8th Ave SW, Calgary (403) 269-8262
+      </p>
+      <p>
+        Best Western Downtown - 1330 8th St SW Calgary, AB (403) 228-6900
+        <br />
+      </p>
+      <p> Nuvo Hotel - 827 12 Ave SW Calgary, AB</p>
+      <p>
+        {" "}
+        Hilton Garden Inn Calgary Downtown - 711 4th St. S.E., Calgary, AB,
+        (587) 352-2020
+      </p>
+      <p>
+        <br />
       </p>
     </div>
   );
