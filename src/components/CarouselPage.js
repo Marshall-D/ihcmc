0dd425ec097339af0bfa30d36fc76d920babe973
d1 <www.didowu@gmail.com>
2023-04-25 18:56:57 +0100

    added images to carousel

--- src/components/CarouselPage.js
+++ src/components/CarouselPage.js
@@ -4,7 +4,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const CarouselPage = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [totalSlides, setTotalSlides] = useState(3);
+  const [totalSlides, setTotalSlides] = useState(4);
   useEffect(() => {
     if (currentSlide === totalSlides - 1) {
       setTimeout(() => setCurrentSlide(0), 3000);
@@ -12,29 +12,32 @@ const CarouselPage = () => {
   }, [currentSlide, totalSlides]);
 
 return (
-  <Carousel 
-  autoPlay={true}
-  interval={3000}
-  showArrows={false}
-  showStatus={false}
-  showThumbs={false}
-  onChange={(index) => setCurrentSlide(index)}
-  selectedItem={currentSlide}
-  onClickItem={(index) => setCurrentSlide(index)}
-  onInit={() => setTotalSlides(3)}>
+  <Carousel
+    autoPlay={true}
+    interval={3000}
+    showArrows={false}
+    showStatus={false}
+    showThumbs={false}
+    showIndicators={false}
+    onChange={(index) => setCurrentSlide(index)}
+    selectedItem={currentSlide}
+    onClickItem={(index) => setCurrentSlide(index)}
+    onInit={() => setTotalSlides(4)}
+  >
     <div>
-      <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/images/25371306/huge/vera_250.gif" />
-      {/* <p className="legend">Legend 1</p> */}
+      <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/headers/3515100/0.jpg" />
     </div>
 
     <div>
-      <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/images/25371306/huge/vera_250.gif" />
-      <p className="legend">Legend 2</p>
+      <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/headers/3515111/fea-2019-white-coat-ceremony.jpg" />
     </div>
 
     <div>
-      <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/images/25371306/huge/vera_250.gif" />
-      <p className="legend">Legend 3</p>
+      <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/headers/3515112/landing-banner.jpg" />
+    </div>
+
+    <div>
+      <img src="https://dk-media.s3.amazonaws.com/media/1vjrh/headers/3515128/midage_group_inside_doctors_001_web_banner_center.jpg" />
     </div>
   </Carousel>
 );
